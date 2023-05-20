071110d44bd8e8a1c658cd47bbefabe569fe783e
CJFaulkner04 <112441692+CJFaulkner04@users.noreply.github.com>
2023-05-19 21:37:34 -0400

    Update makeLast.java

--- makeLast.java
+++ makeLast.java
@@ -1,5 +1,8 @@
 public int[] makeLast(int[] nums) {
+  //create newNums array thats double nums array length
   int[] newNums = new int[nums.length*2];
+  //set last element in newNums to last element in nums
   newNums[newNums.length - 1] = nums[nums.length - 1];
+  //return new nums
   return newNums; 
 }
