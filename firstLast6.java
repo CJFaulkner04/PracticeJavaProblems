53a2c7106f2449e9b9f99eafb2ca86d7059c4c7c
CJFaulkner04 <112441692+CJFaulkner04@users.noreply.github.com>
2023-05-19 21:29:17 -0400

    Update firstLast6.java

--- firstLast6.java
+++ firstLast6.java
@@ -1,4 +1,5 @@
 public boolean firstLast6(int[] nums) {
+  //if the first number in the array is equal to 6 or if the last number is equal to 6 (-1 because arrays start at 0)
   if(nums[0] == 6 || nums[nums.length-1]==6){
     return true;
   }
