624912b8854499a1caf53c600737015be94d759f
CJFaulkner04 <112441692+CJFaulkner04@users.noreply.github.com>
2023-05-19 21:32:09 -0400

    Update love6.java

--- love6.java
+++ love6.java
@@ -1,7 +1,10 @@
 public boolean love6(int a, int b) {
+  //checks to see if a or b is equal to 6
   if(a == 6 || b == 6){
     return true;
-  } else if (a + b == 6 || a - b == 6 || b - a == 6){
+  } 
+  //then checks to see if the sumation or difference is equal to 6
+  else if (a + b == 6 || a - b == 6 || b - a == 6){
     return true;
   }
   return false;
