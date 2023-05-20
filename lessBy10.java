ea3b7567fe68ea3775dacb972657016bca7c0560
CJFaulkner04 <112441692+CJFaulkner04@users.noreply.github.com>
2023-05-19 21:31:13 -0400

    Update lessBy10.java

--- lessBy10.java
+++ lessBy10.java
@@ -1,7 +1,9 @@
 public boolean lessBy10(int a, int b, int c) {
+  //setting difference of all possibilities 
   int x = Math.abs(a - b);
   int y = Math.abs(a - c);
   int z = Math.abs(b - c);
-    
+  
+  //returns the equations boolean
   return x >= 10 || y >= 10 || z >= 10;
 }
