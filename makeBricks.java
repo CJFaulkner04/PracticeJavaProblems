d3c1a2be6910a94443bde5b232868c96e4224de5
CJFaulkner04 <112441692+CJFaulkner04@users.noreply.github.com>
2023-05-19 21:36:31 -0400

    Update makeBricks.java

--- makeBricks.java
+++ makeBricks.java
@@ -1,9 +1,14 @@
 public boolean makeBricks(int small, int big, int goal) {
+  //calc num of big bricks needed
   int need = goal / 5;
-    if (need <= big) {
-        goal -= need * 5;
-    } else {
-        goal -= big * 5;
-    }
-    return goal <= small;
+  //subtract length of bricks from goal if num of big bricks needed is less than or equal to num of big bricks
+  if (need <= big) {
+    goal -= need * 5;
+  }
+  //else subtract the length of all big bricks from goal
+  else {
+    goal -= big * 5;
+  }
+  
+  return goal <= small;
 }
